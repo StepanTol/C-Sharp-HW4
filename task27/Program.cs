9f1fb6d5511c5e5799ffe92c6d4f1e9b14bc9bee
Stepan <tstepa037@gmail.com>
2023-01-05 13:07:05 +0300

    task 27

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите число: "); 
+int N = int.Parse(Console.ReadLine()!);
+void arrayN (int[] col)
+{   
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(0,N);       
+    }   
+} 
+void printArray (int[] col)
+{   
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.WriteLine($"{col[i]} ");
+    }    
+} 
+int [] array = new int[8];
+arrayN(array);
+printArray(array);
